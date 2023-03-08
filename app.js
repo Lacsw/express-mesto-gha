926dafa76afddc89024252e4f3600a6516e2e978
Roman Frolov <frolloff1@yandex.ru>
2023-03-08 11:14:52 +0300

    ref: перенести 404 роут в корневой роутер

--- app.js
+++ app.js
@@ -12,14 +12,10 @@ app.use((req, res, next) => {
   req.user = {
     _id: '6404abf703625ca6cc1575a1',
   };
-
   next();
 });
 
 app.use('/', router);
-app.all('*', (req, res) => {
-  res.status(404).send({ message: 'Страница не найдена' });
-});
 
 app.listen(3000, () => {
   console.log('Server started');
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 const router = require('express').Router();
+const { HTTP_STATUS_NOT_FOUND } = require('http2').constants;
 
 const usersRouter = require('./users');
 const cardsRouter = require('./cards');
 
 router.use('/users', usersRouter);
 router.use('/cards', cardsRouter);
+router.all('*', (req, res) => {
+  res.status(HTTP_STATUS_NOT_FOUND).send({ message: 'Страница не найдена' });
+});
 
 module.exports = router;

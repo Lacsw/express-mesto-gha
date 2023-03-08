c7e7cdb25a509338477a2b943cebcae0d5752f50
Roman Frolov <frolloff1@yandex.ru>
2023-03-08 17:52:48 +0300

    feat: создать файл конфига

--- app.js
+++ app.js
@@ -2,21 +2,20 @@ const express = require('express');
 const mongoose = require('mongoose');
 const router = require('./routes/index');
 
-const app = express();
+const { PORT, DATABASE_URL } = require('./config');
 
-mongoose.connect('mongodb://localhost:27017/mestodb');
+const app = express();
+mongoose.connect(DATABASE_URL);
 
 app.use(express.json());
-
 app.use((req, res, next) => {
   req.user = {
     _id: '6404abf703625ca6cc1575a1',
   };
   next();
 });
-
 app.use('/', router);
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log('Server started');
 });

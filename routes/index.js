f8ead45dface442c0320c7f5d0e91eb7021b67ca
Roman Frolov <frolloff1@yandex.ru>
2023-03-24 12:28:00 +0300

    feat: добавить роут logout с очисткой куки

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,9 @@ const auth = require('../middlewares/auth');
 
 router.use('/', authRouter);
 router.use(auth);
+router.post('/signout', (req, res) => {
+  res.clearCookie('jwt').send({ message: 'Выход' });
+});
 
 router.use('/users', usersRouter);
 router.use('/cards', cardsRouter);
